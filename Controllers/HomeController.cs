c8e4b44ac71f645191549172f061ef8470e9cf95
Jeay <jeay2001@gmail.com>
2024-09-21 12:30:36 +0200

    Layout aangepast navbar top en side

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using Verzamelwoede.Data;
 using Verzamelwoede.Models;
 
 namespace Verzamelwoede.Controllers
@@ -7,12 +9,38 @@ namespace Verzamelwoede.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly VerzamelwoedeDB _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, VerzamelwoedeDB context)
         {
             _logger = logger;
+            _context = context;
         }
 
+        // GET: /Home/Items
+        public async Task<IActionResult> Items()
+        {
+            var items = await _context.Items.ToListAsync();
+            return View(items);
+        }
+
+        // GET: /Home/Item/5
+        public async Task<IActionResult> Item(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
         public IActionResult Index()
         {
             return View();
@@ -28,5 +56,6 @@ namespace Verzamelwoede.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
